13148dd2dc5dcf104b727ece1b6fbbac6d814300
Roman Solomud <romaslmd@gmail.com>
2014-10-30 17:25:18 +0200

    [~] + prev commit

--- app/models/fields_setting.rb
+++ app/models/fields_setting.rb
@@ -5,11 +5,11 @@ class FieldsSetting < ActiveRecord::Base
 	validates(:object, presence: true)
 
 	def disallowed_fields
-		selected.split(",") rescue []
+		@disallowed_fields ||= selected.split(",") rescue []
 	end
 
 	def disallowed_fields= fields
-		self.selected = fields.join(",")
+		self.selected = fields.empty? ? nil : disallowed_fields.concat(fields).uniq.join(",")
 	end
 
 end
