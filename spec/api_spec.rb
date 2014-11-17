c0481a0727d29d90e1801e07bedac472b80f4e50
Roman Solomud <romaslmd@gmail.com>
2014-11-17 14:20:51 +0200

    [~] remove comments from prev

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -3,23 +3,23 @@ describe(Application) do
 
   let(:api_options) { { :config => File.expand_path(File.join(File.dirname(__FILE__), "..", "config", "application.rb")) } }
 
-  # it("should respond with json actions list") do
-  #   with_api(Application, api_options) do
-  #     get_request(path: "/api/json_actions") do |c|
-  #       response = JSON.parse(c.response)
-  #       expect(response).to have_key("actions")
-  #     end
-  #   end
-  # end
+  it("should respond with json actions list") do
+    with_api(Application, api_options) do
+      get_request(path: "/api/json_actions") do |c|
+        response = JSON.parse(c.response)
+        expect(response).to have_key("actions")
+      end
+    end
+  end
   
-  # it("should respond with swagger doc") do
-  # 	with_api(Application, api_options) do
-  #     get_request(path: "/docs") do |c|
-  #       response = JSON.parse(c.response)
-  #       expect(c.response_header.status).to eq(200)
-  #       expect(response.count).not_to eq(0)
-  #     end
-  #   end
-  # end
+  it("should respond with swagger doc") do
+  	with_api(Application, api_options) do
+      get_request(path: "/docs") do |c|
+        response = JSON.parse(c.response)
+        expect(c.response_header.status).to eq(200)
+        expect(response.count).not_to eq(0)
+      end
+    end
+  end
 
 end
--- spec/api_tariffs_spec.rb
+++ spec/api_tariffs_spec.rb
@@ -5,32 +5,32 @@ describe(Application) do
   let(:token) { Faker::Internet.ip_v6_address.tr(":", "") }
   let(:user) { create(:user, password: "temp_password") }
 
-  # it("should respond with tariffs list to admin") do
-  #   with_api(Application, api_options) do
-  #     get_request(path: "/api/tariffs/#{token}", query: { is_admin: true }) do |c|
-  #       response = JSON.parse(c.response)
-  #       expect(response).to have_key("tariffs")
-  #       expect(response["tariffs"].count).to eq(Tariff.count)
-  #     end
-  #   end
-  # end
+  it("should respond with tariffs list to admin") do
+    with_api(Application, api_options) do
+      get_request(path: "/api/tariffs/#{token}", query: { is_admin: true }) do |c|
+        response = JSON.parse(c.response)
+        expect(response).to have_key("tariffs")
+        expect(response["tariffs"].count).to eq(Tariff.count)
+      end
+    end
+  end
 
-  # it("should respond with tariffs list to user") do
-  #   with_api(Application, api_options) do
-  #     get_request(path: "/api/tariffs/#{token}", query: { user_id: user.id }) do |c|
-  #       response = JSON.parse(c.response)
-  #       expect(response).to have_key("tariffs")
-  #       expect(response["tariffs"].count).to eq(Tariff.count)
-  #     end
-  #   end
-  # end
+  it("should respond with tariffs list to user") do
+    with_api(Application, api_options) do
+      get_request(path: "/api/tariffs/#{token}", query: { user_id: user.id }) do |c|
+        response = JSON.parse(c.response)
+        expect(response).to have_key("tariffs")
+        expect(response["tariffs"].count).to eq(Tariff.count)
+      end
+    end
+  end
 
-  # it("should raise unauthorized if user not found") do
-  #   with_api(Application, api_options) do
-  #     get_request(path: "/api/tariffs/#{token}", query: { user_id: nil }) do |c|
-  #       expect(c.response_header.status).to eq(401)
-  #     end
-  #   end
-  # end
+  it("should raise unauthorized if user not found") do
+    with_api(Application, api_options) do
+      get_request(path: "/api/tariffs/#{token}", query: { user_id: nil }) do |c|
+        expect(c.response_header.status).to eq(401)
+      end
+    end
+  end
 
 end
