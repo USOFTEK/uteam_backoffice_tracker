01cd22d1d1212b401dc278c615723e17ebe0af0f
Roman Solomud <romaslmd@gmail.com>
2014-11-28 10:52:22 +0200

    [~] add default value for tariff->tv

--- db/migrate/29_add_tv_package_id_to_tariffs_table.rb
+++ db/migrate/29_add_tv_package_id_to_tariffs_table.rb
@@ -1,6 +1,6 @@
 class AddTvPackageIdToTariffsTable < ActiveRecord::Migration
   def up
-    add_column(:tariffs, :tv_package_id, :integer, limit: 11)
+    add_column(:tariffs, :tv_package_id, :integer, limit: 11, default: 0)
   end
   def down
     remove_column(:tariffs, :tv_package_id)
